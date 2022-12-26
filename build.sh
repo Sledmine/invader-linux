82039d4395c3e1ebdabd4f5ccd7570d0ab14f22f
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-12-25 18:39:30 -0600

    add build script, update ubuntu docker file

---
+++ build.sh
@@ -0,0 +1,13 @@
+cd /workspace && rm -rf ext/riat && \
+    git clone https://github.com/SnowyMouse/riat ext/riat
+
+# Add missing climits include
+sed -i '2 i\#include <climits>' /workspace/src/tag/parser/compile/model.cpp
+
+# Build invader
+cd /workspace && mkdir build ; cd build && cmake .. && make -j $(nproc)
+
+# Build six-shooter
+cd /invader && git clone https://github.com/SnowyMouse/six-shooter && \
+cd six-shooter && mkdir build ; cd build && cmake .. && make -j $(nproc) && \
+cp six-shooter /workspace/build
