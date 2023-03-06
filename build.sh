982a23cd0f5b53de05a04b20243e7002482d8123
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-03-06 02:05:00 -0600

    check if climits was already included

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 cd /workspace && rm -rf ext/riat && \
     git clone https://github.com/SnowyMouse/riat ext/riat
 
-# Add missing climits include
+# Add missing climits include if ot does not exist
+grep -q climits /workspace/src/tag/parser/compile/model.cpp || \
 sed -i '2 i\#include <climits>' /workspace/src/tag/parser/compile/model.cpp
 
 # Build invader
